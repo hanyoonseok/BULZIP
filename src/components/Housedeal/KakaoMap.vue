7832c118d4084ffc4e7242dda5fda881b47cd482
choiswonspec <csw31915@gmail.com>
2022-05-21 18:55:58 +0900

    18:55

--- src/components/Housedeal/KakaoMap.vue
+++ src/components/Housedeal/KakaoMap.vue
@@ -3,12 +3,15 @@
 </template>
 <script src="https://developers.kakao.com/sdk/js/kakao.js"></script>
 <script>
+import http from "@/api/http.js";
+
 export default {
   name: "KakaoMap",
   mounted() {
     window.kakao && window.kakao.maps ? this.initMap() : this.addScript();
   },
   methods: {
+    ////////////////////////////////////////////////////
     initMap() {
       var container = document.getElementById("map");
       var options = {
@@ -21,7 +24,51 @@ export default {
         position: map.getCenter(),
       });
       marker.setMap(map);
+
+      ///////////////////클러스터 부분 ///////////////////
+      // https://apis.map.kakao.com/web/sample/basicClusterer/
+      var clusterer = new kakao.maps.MarkerClusterer({
+        map: map, // 마커들을 클러스터로 관리하고 표시할 지도 객체
+        averageCenter: true, // 클러스터에 포함된 마커들의 평균 위치를 클러스터 마커 위치로 설정
+        minLevel: 10, // 클러스터 할 최소 지도 레벨
+        /////////////클릭 이벤트 추가//////////////////
+        disableClickZoom: true, // 클러스터 마커를 클릭했을 때 지도가 확대되지 않도록 설정한다
+      });
+      // 데이터를 가져와 마커를 생성하고 클러스터러 객체에 넘겨줍니다
+
+      // 이부분에서 데이터를 가져와야함.........
+      // 거래 매물 데이터들을 positions 로 가져와야함.
+
+      // 데이터에서 좌표 값을 가지고 마커를 표시합니다
+      // 마커 클러스터러로 관리할 마커 객체는 생성할 때 지도 객체를 설정하지 않습니다
+      var markers = $(data.positions).map(function (i, position) {
+        return new kakao.maps.Marker({
+          position: new kakao.maps.LatLng(position.lat, position.lng), // 위도 경도 입력
+        });
+      });
+
+      // 클러스터러에 마커들을 추가합니다
+      clusterer.addMarkers(markers);
+
+      // 마커 클러스터러에 클릭이벤트를 등록합니다
+      // 마커 클러스터러를 생성할 때 disableClickZoom을 true로 설정하지 않은 경우
+      // 이벤트 헨들러로 cluster 객체가 넘어오지 않을 수도 있습니다
+      // kakao.maps.event.addListener(
+      //   clusterer,
+      //   "clusterclick",
+      //   function (cluster) {
+      //     // 현재 지도 레벨에서 1레벨 확대한 레벨
+      //     var level = map.getLevel() - 1;
+
+      //     // 지도를 클릭된 클러스터의 마커의 위치를 기준으로 확대합니다
+      //     map.setLevel(level, { anchor: cluster.getCenter() });
+      //   }
+      // );
+      ////////////////////////////////////////////////////////
     },
+
+    //////////////////////////////////////////////////////
+
     addScript() {
       const script = document.createElement("script");
       /* global kakao */
