5296bfeba19bdb98aa8b74004c5b99a14f600b59
choiswonspec <csw31915@gmail.com>
2022-05-22 15:46:56 +0900

    15:46

---
+++ src/components/Housedeal/items/TimeseriesList.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="hero is-success is-fullheight">
+    <div class="hero-body">
+      <div class="container">
+        <h1>막대 그래프</h1>
+        <ul>
+          <bar-chart></bar-chart>
+        </ul>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import BarChart from "@/components/Charts/BarChart.vue";
+export default {
+  components: { BarChart },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style></style>
