47cd363f7c828c9d6a3845f3cfaef2b42ca3d4ad
choiswonspec <csw31915@gmail.com>
2022-05-24 15:29:55 +0900

    15:29

--- src/components/Housedeal/items/RecommendedList.vue
+++ src/components/Housedeal/items/RecommendedList.vue
@@ -1,6 +1,6 @@
 <template>
   <article id="main-list-container">
-    <div class="main-list" v-if="status === 0 && !selectedItem">
+    <div class="main-list" v-if="status === -1 && !items">
       <form id="filter-container" v-on:submit.prevent="search">
         <input
           type="text"
@@ -32,11 +32,7 @@
       </div>
     </div>
 
-    <HouseDetail
-      v-if="selectedItem"
-      @backToList="selectedItem = null"
-      :item="selectedItem"
-    />
+    <HouseDetail v-if="items" @backToList="items = null" :item="items" />
 
     <HouseLike v-if="status < 0" />
   </article>
@@ -47,42 +43,29 @@ import http from "@/api/http.js";
 import HouseItem from "@/components/Housedeal/items/HouseItem.vue";
 import HouseLike from "@/components/Housedeal/items/HouseLike.vue";
 import HouseDetail from "@/components/Housedeal/items/HouseDetail.vue";
+import { mapState } from "vuex";
 
 export default {
   data() {
     return {
-      selectedItem: null,
-      items: [],
+      //selectedItem: null,
+      items: [], // 여기에 추천 리스트 담긴다.
 
-      status: 0, //0=전체목록, -1=관심목록, > 1 상세조회
+      status: -1, //0=전체목록, -1=관심목록, > 1 상세조회
     };
   },
+  computed: {
+    ...mapState("userStore", ["userInfo"]), // 유저아이디 : this.userInfo.userId
+  },
+
   created() {
-    this.$EventBus.$on("getListByLatLng", (range) => {
-      this.getListByLatLng(range);
-    });
-    if (this.$route.params.keyword) {
-      this.keyword = this.$route.params.keyword;
-      this.getList(this.keyword);
-    } else {
-      this.getList();
-    }
+    this.getList(userInfo.userId);
   },
   methods: {
-    getList(user_id = "") {
+    getList(user_id) {
       http.get(`/housedeal/recommend/${user_id}`).then((resp) => {
         this.items = this.items.concat(resp.data);
       });
-    },
-
-    getListByLatLng(list) {
-      this.items = list;
-    },
-
-    search() {
-      const keyword = this.keyword;
-      this.items = [];
-      this.getList(keyword);
     },
     selectOne(selectedItem) {
       this.selectedItem = selectedItem;
